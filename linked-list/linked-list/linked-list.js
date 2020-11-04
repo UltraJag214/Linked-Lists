a2a86beb8713814f8aa9b41fe008228caac19289
Hayden Gibbons <hayden@Lauras-MacBook-Air.local>
2020-11-04 07:34:58 -0700

    Linked list assingment

---
+++ linked-list/linked-list/linked-list.js
@@ -0,0 +1,62 @@
+class LinkedListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+
+  insert (val) {
+    let newNode = new LinkedListNode(val);
+    if (this.head == null) {
+      this.head = newNode;
+    } else {
+      let node = this.head;
+      while(node.next) {
+        node = node.next;
+      }
+      node.next = newNode;
+    }
+  }
+
+  remove(val) {
+    let node = this.head;
+    let previous = null;
+    while(node) {
+      if(node.data == val) {
+        if(previous != null) {
+          previous.next = node.next
+        } else {
+          this.head = null;
+        }
+        return;
+      }
+      previous = node;
+      node = node.next;
+    }
+  }
+
+  find(val) {
+    let node = this.head;
+    while(node) {
+      if(node.data == val) return true;
+      node = node.next;
+    }
+    return false;
+  }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+    while(node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+}
+
